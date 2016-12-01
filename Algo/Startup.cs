95cce193aff7261ee5aff1a532098f669976c094
Neil Jones <neil.j.neil@gmail.com>
2016-12-01 07:05:47 -0600

    Presentation Website

---
+++ Algo/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Algo.Startup))]
+namespace Algo
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
